724f8a464581b06c66cbbe22b75ed8332bb50700
Nexiauk <149090274+Nexiauk@users.noreply.github.com>
2025-06-16 20:09:43 +0100

    Remove env.py from repository and add to .gitignore

--- env.py
+++
@@ -1,8 +0,0 @@
-import os
-os.environ.setdefault(
-    "DATABASE_URL", "postgresql://neondb_owner:npg_jCDzS6fYoxH3@ep-quiet-river-a26bpmb9.eu-central-1.aws.neon.tech/ion_slate_small_244646"
-)
-
-os.environ.setdefault(
-    "SECRET_KEY", "6jy=0$mdb205j%ko!j@0oqhh+04afocod)obq-3r9b&f$p#39$"
-)
