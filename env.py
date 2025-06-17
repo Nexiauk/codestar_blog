4999999c74bbd47e1a0af00b5ef10da308ae5e57
Nexiauk <149090274+Nexiauk@users.noreply.github.com>
2025-06-17 19:49:04 +0100

    added base.html and index.html, as well as a styles directory and file. experimented with blocks

---
+++ env.py
@@ -0,0 +1,8 @@
+import os
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_jCDzS6fYoxH3@ep-quiet-river-a26bpmb9.eu-central-1.aws.neon.tech/ion_slate_small_244646"
+)
+
+os.environ.setdefault(
+    "SECRET_KEY", "a62w6v-vb@m!eoe3e(($gj&m7o@3&17zhb6*_f3jg)9&r@&sgo"
+)
